a74620658e9301c607ae2248314ef3949c134195
Daniil Yarockii <yaroc.d@gmail.com>
2022-06-28 13:20:18 +0300

    read params

--- src/demo_api_requests.py
+++ src/demo_api_requests.py
@@ -6,14 +6,16 @@ import requests
 
 
 def main():
-
     task_id = os.getenv("TASK_ID", None)
 
+    data = {'inference_image_id': '123'}
     if task_id is not None:
         api = sly.Api.from_env()
-        response = api.task.send_request(task_id=task_id, method='/test-request/', data={})
+        response = api.task.send_request(task_id=task_id, method='/test-request-recommended/', data=data)
+        response = api.task.send_request(task_id=task_id, method='/test-request-raw/', data=data)
     else:
-        response = requests.post("http://127.0.0.1:8000/test-request/", data={}, timeout=1)
+        response = requests.post("http://127.0.0.1:8000/test-request-recommended/", json=data, timeout=1)
+        response = requests.post("http://127.0.0.1:8000/test-request-raw/", json=data, timeout=1)
     print("APP returns data:")
     print(response)
 
--- src/main.py
+++ src/main.py
@@ -1,18 +1,33 @@
 import supervisely as sly
-from starlette.requests import Request
+from fastapi import Request, FastAPI
 from supervisely import logger
 
 import src.sly_functions as f
 import src.sly_globals as g
 
 
-logger.info("Application has been started")
+from pydantic import BaseModel
 
 
-@g.app.post('/test-request/')
-def test_request(request: Request):
-    logger.info("Test request received")
-    print(request)
+class Item(BaseModel):
+    inference_image_id: int
+
+
+@g.app.post('/test-request-recommended/')
+async def test_request_recommended(inference_item: Item):
+    logger.info(f"{test_request_recommended.__name__} received a message")
+    logger.info(f'{inference_item.inference_image_id=}')
     return {'status_code': 200, 'data': 'OK'}
 
 
+@g.app.post('/test-request-raw/')
+async def test_request_raw(request: Request):
+    logger.info(f"{test_request_raw.__name__} received a message")
+    # logger.info("image_id_received: %s", inference_item.inference_image_id)
+    raw_body = await request.json()
+    logger.info(f"{raw_body=}")
+
+    return {'status_code': 200, 'data': 'OK'}
+
+
+logger.info("Application has been started")
